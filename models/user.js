bfc0ea8e14ea373c8c7d3982e3fe1e3603ebe556
Kiran R <kiran.r1497@gmail.com>
2026-05-05 17:23:02 +0530

    fourth commit - add to cart and delete from cart

--- models/user.js
+++ models/user.js
@@ -30,6 +30,39 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.methods.addToCart = function () {};
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  });
+
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    // Product already in cart → increase quantity
+    updatedCartItems[cartProductIndex].quantity += 1;
+  } else {
+    // New product → add to cart
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: 1,
+    });
+  }
+
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+
+  this.cart = updatedCart;
+  return this.save();
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
